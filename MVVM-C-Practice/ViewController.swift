fe7e65de1f01d8023191edb84d2b5c7bbeef68af
유인호 <yuiop1029@naver.com>
2024-05-28 20:24:25 +0900

    Initial Commit

---
+++ MVVM-C-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C-Practice
+//
+//  Created by A_Mcflurry on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
